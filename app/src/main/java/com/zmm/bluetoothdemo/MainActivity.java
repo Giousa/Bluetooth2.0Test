4b20c117557f0ad9f36ac134429ebac07e51bd6a
Giousa <65489469@qq.com>
2017-03-27 14:50:21 +0800

    蓝牙2.0和4.0搜索分开

--- app/src/main/java/com/zmm/bluetoothdemo/MainActivity.java
+++ app/src/main/java/com/zmm/bluetoothdemo/MainActivity.java
@@ -3,8 +3,10 @@ package com.zmm.bluetoothdemo;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -17,17 +19,36 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
+/**
+ * 蓝牙2.0 搜索、连接
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static String TAG = MainActivity.class.getSimpleName();
     private static final UUID MY_UUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
-    private static int REQUEST_ENABLE_BT = 1;
+
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+
     @InjectView(R.id.recycler_view)
     RecyclerView mRecyclerView;
     private BluetoothAdapter mBluetoothAdapter;
@@ -35,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
     private static final long SCAN_PERIOD = 10000;
     private List<BluetoothDevice> mBluetoothDeviceList;
     private MyAdapter mMyAdapter;
+    private BluetoothChatService mChatService = null;
+
 
 
     @Override
@@ -45,14 +68,12 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.inject(this);
         mHandler = new Handler();
         DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);//display = getWindowManager().getDefaultDisplay();display.getMetrics(dm)（把屏幕尺寸信息赋值给DisplayMetrics dm）;
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
         int widthPixels = dm.widthPixels;
         int heightPixels = dm.heightPixels;
         Log.d(TAG, "width = " + widthPixels + ",height = " + heightPixels);
 
-
-        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
     private void initView() {
@@ -71,11 +92,29 @@ public class MainActivity extends AppCompatActivity {
                 bluetoothConnect(position);
             }
         });
+
+        // 获得一个已经配对的蓝牙设备的set集合
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                Log.d(TAG,"已配对："+device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            Toast.makeText(this, "没有已配对的设备", Toast.LENGTH_SHORT).show();
+        }
+
+
+        //当发现一个新的蓝牙设备时注册广播
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(mReceiver, filter);
+
+        //当搜索完毕后注册广播
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filter);
     }
 
     private void bluetoothConnect(int position) {
         BluetoothDevice device = mBluetoothDeviceList.get(position);
-
     }
 
 
@@ -97,7 +136,6 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.btn_stop:
                 Toast.makeText(this, "Stop Scan", Toast.LENGTH_SHORT).show();
-                mBluetoothAdapter.stopLeScan(mLeScanCallback);
                 break;
             case R.id.btn_connect:
 
@@ -106,27 +144,68 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void scanLeDevice() {
-        mBluetoothAdapter.startLeScan(mLeScanCallback);
-        invalidateOptionsMenu();
+        if (mBluetoothAdapter.isDiscovering()) {
+            mBluetoothAdapter.cancelDiscovery();
+        }
+        mBluetoothAdapter.startDiscovery();
     }
 
-    private BluetoothAdapter.LeScanCallback mLeScanCallback =
-            new BluetoothAdapter.LeScanCallback() {
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG,"广播开启:");
+            // 当发现一个新的蓝牙设备时
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.d(TAG,"device name = "+device.getName()+",address = "+device.getAddress());
+                mMyAdapter.add(device, 0);
+                mRecyclerView.scrollToPosition(0);
+                // If it's already paired, skip it, because it's been listed already
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    Log.d(TAG,"未配对： "+ device.getName() + "\n" + device.getAddress());
 
-                @Override
-                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            //只将名为“GongJinMedical_0”的蓝牙设备添加的显示列表当中
-                            Log.d(TAG, "name = " + device.getName() + ",address = " + device.getAddress());
-//                            mBluetoothDeviceList.add(bluetoothBean);
-                            mMyAdapter.add(device, 0);
-                            mRecyclerView.scrollToPosition(0);
-                        }
-                    });
                 }
-            };
+                // When discovery is finished, change the Activity title
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.d(TAG,"广播结束:");
+                if (mBluetoothDeviceList.size() == 0) {
+                    Toast.makeText(MainActivity.this, "没有发现新设备", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        } else {
+            if (mChatService == null) {
+                mChatService = new BluetoothChatService(this, mHandler);
+            }
+        }
+    }
+
+    @Override
+    public synchronized void onResume()
+    {
+        super.onResume();
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null)
+        {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE)
+            {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
+        }
+    }
 
     private void openBlueTooth() {
 
@@ -139,6 +218,10 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
         }
 
+        if (mChatService == null) {
+            mChatService = new BluetoothChatService(this, mHandler);
+        }
+
 
     }
 
@@ -146,6 +229,19 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_ENABLE_BT) {
             Log.d(TAG, "---蓝牙已开启---");
         }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Stop the Bluetooth chat services
+        if (mChatService != null) mChatService.stop();
+
+        // Make sure we're not doing discovery anymore
+        if (mBluetoothAdapter != null) {
+            mBluetoothAdapter.cancelDiscovery();
+        }
+        // Unregister broadcast listeners
+        this.unregisterReceiver(mReceiver);
     }
 
 }
---
+++ app/src/main/java/com/zmm/bluetoothdemo/SecondActivity.java
@@ -0,0 +1,156 @@
+package com.zmm.bluetoothdemo;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+/**
+ * Description: 4.0搜索方法
+ * Author:zhangmengmeng
+ * Date:2017/3/27
+ * Time:下午2:20
+ */
+
+public class SecondActivity extends AppCompatActivity{
+    private static String TAG = MainActivity.class.getSimpleName();
+    private static final UUID MY_UUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
+    private static int REQUEST_ENABLE_BT = 1;
+    @InjectView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+    private BluetoothAdapter mBluetoothAdapter;
+    private Handler mHandler;
+    private static final long SCAN_PERIOD = 10000;
+    private List<BluetoothDevice> mBluetoothDeviceList;
+    private MyAdapter mMyAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+        mHandler = new Handler();
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);//display = getWindowManager().getDefaultDisplay();display.getMetrics(dm)（把屏幕尺寸信息赋值给DisplayMetrics dm）;
+        int widthPixels = dm.widthPixels;
+        int heightPixels = dm.heightPixels;
+        Log.d(TAG, "width = " + widthPixels + ",height = " + heightPixels);
+
+
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+    }
+
+    private void initView() {
+        mBluetoothDeviceList = new ArrayList<>();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mMyAdapter = new MyAdapter(mBluetoothDeviceList);
+        mRecyclerView.setAdapter(mMyAdapter);
+        mMyAdapter.setOnItemSelectedListener(new MyAdapter.OnItemSelectedListener() {
+            @Override
+            public void OnItemSelect(int position) {
+                BluetoothDevice bluetoothDevice = mBluetoothDeviceList.get(position);
+                String show = "name = "+bluetoothDevice.getName()+",address = "+bluetoothDevice.getAddress();
+                Toast.makeText(SecondActivity.this,show, Toast.LENGTH_SHORT).show();
+                bluetoothConnect(position);
+            }
+        });
+    }
+
+    private void bluetoothConnect(int position) {
+        BluetoothDevice device = mBluetoothDeviceList.get(position);
+    }
+
+
+    @OnClick({R.id.btn_open, R.id.btn_query, R.id.btn_stop, R.id.btn_connect})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_open:
+                openBlueTooth();
+                break;
+            case R.id.btn_query:
+                Toast.makeText(this, "Start Scan", Toast.LENGTH_SHORT).show();
+                if(mBluetoothDeviceList != null  && mBluetoothDeviceList.size()>0){
+                    mBluetoothDeviceList.clear();
+                    mBluetoothDeviceList = null;
+                }
+                initView();
+                scanLeDevice();
+                break;
+
+            case R.id.btn_stop:
+                Toast.makeText(this, "Stop Scan", Toast.LENGTH_SHORT).show();
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                break;
+            case R.id.btn_connect:
+
+                break;
+        }
+    }
+
+    private void scanLeDevice() {
+        mBluetoothAdapter.startLeScan(mLeScanCallback);
+        invalidateOptionsMenu();
+    }
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //只将名为“GongJinMedical_0”的蓝牙设备添加的显示列表当中
+                            Log.d(TAG, "name = " + device.getName() + ",address = " + device.getAddress());
+//                            mBluetoothDeviceList.add(bluetoothBean);
+                            mMyAdapter.add(device, 0);
+                            mRecyclerView.scrollToPosition(0);
+                        }
+                    });
+                }
+            };
+
+    private void openBlueTooth() {
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(SecondActivity.this, "蓝牙不支持", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            Log.d(TAG, "---蓝牙已开启---");
+        }
+    }
+
+}
